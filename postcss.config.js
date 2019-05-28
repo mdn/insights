567201658bb1ed9bd84a1a6dd670eae158eb9bad
joshJarr <josh.jarvis@potatolondon.com>
2019-05-28 14:32:10 +0100

    Merge master build changes.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
   plugins: [
+    purgecss({
+      content: ['./**/*.html']
+    }),
     autoprefixer({
       browsers: 'last 2 versions'
     })
