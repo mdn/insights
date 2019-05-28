6bffbf1a98a2821232f2aa3f160122d4285b624d
joshJarr <josh.jarvis@potatolondon.com>
2019-05-28 15:26:12 +0100

    Introduce common components and styles from mdn/kuma.]

--- webpack.common.js
+++ webpack.common.js
@@ -64,6 +64,9 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+            },
           }
         ]
       }
