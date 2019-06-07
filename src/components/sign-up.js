ba23a256bf5dfcc3f749fd5f4f8501c2fa74cc92
Matt Hall <matth@potatolondon.com>
2019-06-07 13:12:57 +0100

    Various updates throughout the codebase

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -5,9 +5,19 @@ class SignUp {
     }
 
     this.element = element;
-    this.submitButton = element.querySelector('input[type="submit"]');
+    this.emailInput = element.querySelector('input[name="email"]');
+    this.privacyInput = element.querySelector('input[name="privacy"]');
+    this.submitButton = element.querySelector('button[type="submit"]');
+    this.successMessage = document.querySelector('.survey-sign-up__success');
 
-    element.addEventListener('submit', this.handleSubmit.bind(this));
+    this.element.addEventListener('submit', this.handleSubmit.bind(this));
+    this.emailInput.addEventListener('focus', this.handleInputFocus.bind(this));
+  }
+
+  handleInputFocus() {
+    this.privacyInput
+      .closest('.survey-sign-up__privacy')
+      .classList.remove('display-none');
   }
 
   handleSubmit(event) {
@@ -21,13 +31,16 @@ class SignUp {
     request.onload = () => {
       console.log('Done'); // eslint-disable-line
       console.log(request.response); // eslint-disable-line
+
+      this.element.classList.add('display-none');
+      this.successMessage.classList.remove('display-none');
     };
 
     request.onerror = () => {
       console.log('Error'); // eslint-disable-line
     };
 
-    request.open('POST', 'https://httpbin.org/post', false);
+    request.open('POST', this.element.getAttribute('action'), false);
     request.send(formData);
   }
 }
