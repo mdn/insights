d704daef1e6854b7c3fe8e6f0a397d9e13e66be2
Schalk Neethling <sneethling@mozilla.com>
2020-12-14 11:55:36 +0200

    fix: new landing page (#48)

--- src/utils/breakpoints.test.js
+++
@@ -1,5 +0,0 @@
-import { BREAKPOINT_CHANGE } from './breakpoints.js';
-
-test('should export a constant for the breakpoint change custom event type', () => {
-  expect(BREAKPOINT_CHANGE).toBe('breakpointchange');
-});
