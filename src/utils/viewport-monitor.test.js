c27d42474e64f59dbd5da86848fe22937c716d49
Schalk Neethling <sneethling@mozilla.com>
2020-12-14 11:50:20 +0200

    fix: new landing page
    Add new landing page for MDN Web Docs Web DNA reports
    
    fix #46

--- src/utils/viewport-monitor.test.js
+++
@@ -1,9 +0,0 @@
-import { APPEARED, DISAPPEARED } from './viewport-monitor.js';
-
-test('should export a constant for the appeared custom event type', () => {
-  expect(APPEARED).toBe('appeared');
-});
-
-test('should export a constant for the disappeared custom event type', () => {
-  expect(DISAPPEARED).toBe('disappeared');
-});
