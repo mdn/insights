b848a71acb5bf23175f06875581830cf852729d0
Schalk Neethling <sneethling@mozilla.com>
2020-12-16 10:19:53 +0200

    update readme (#53)
    Update the README file with instrctions on running locally and building for production.
    This also adds and resolves known issues with the production build.

---
+++ webpack.config.prod.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const ASSET_PATH = process.env.ASSET_PATH || "/";
+
+module.exports = {
+  entry: {
+    minimalist: "./node_modules/@mdn/minimalist/sass/mdn-minimalist.scss",
+    insights: "./sass/insights.scss",
+  },
+  output: {
+    path: path.resolve(__dirname, "public", "css"),
+  },
+  mode: "production",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ASSET_PATH,
+            },
+          },
+          "css-loader",
+          "resolve-url-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|svg)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "../assets/",
+          publicPath: "../assets/",
+        },
+      },
+    ],
+  },
+  watch: false,
+};
